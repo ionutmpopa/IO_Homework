9a91c415430a2cc0fba5440e96e7d1432a7abf7e
unknown <I.m. Popa@DESKTOP-P5HEJEF>
2019-01-01 12:04:54 +0200

    Updated with file separator and constant for the file name string

--- src/com/iopo/BiathlonStanding.java
+++ src/com/iopo/BiathlonStanding.java
@@ -1,11 +1,15 @@
 package com.iopo;
 
+import java.io.File;
+
 public class BiathlonStanding {
+
+    private static final String MY_FILE = "resources" + File.separator + "Athletes.csv";
 
     public static void main(String[] args) {
 
         ReadFromCSV readMyData = new ReadFromCSV();
-        readMyData.readData("resources/Athletes.csv");
+        readMyData.readData(MY_FILE);
         readMyData.printAthletes();
 
     }
